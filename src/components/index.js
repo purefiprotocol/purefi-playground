e302ab00f25e9c242d96d8b9023cb1caf7800f68
Mikhail Radchenko <mradchenko@purefi.io>
2025-03-21 12:42:52 +0300

    feat: new app

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { Layout } from './Layout';
-export { Navbar } from './Navbar';
-export { Form } from './Form';
-export { TxnToastSuccess } from './TxnToast';
--- src/hooks/useWallet.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { WalletContext } from '../context';
-
-const useWallet = () => {
-  const wallet = useContext(WalletContext);
-  return wallet;
-};
-
-export default useWallet;
