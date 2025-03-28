9bfd2824af65bcf50cce91947d92e143bbe1a329
Mikhail Radchenko <mradchenko@purefi.io>
2025-03-28 19:40:47 +0300

    chore: tex

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC, useEffect, useRef, useState } from 'react';
 import {
   PureFI,
